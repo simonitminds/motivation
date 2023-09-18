b9dbf02eb66893175862fdb214433686e47d165f
Simon Bundgaard-Egeberg <seg@it-minds.dk>
2023-09-18 15:20:18 +0200

    feat: added the page to create

---
+++ src/App2.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { fly } from "svelte/transition";
+  import { motivationalQuotesNorwegian } from "./lib/the_quotes";
+  let currentQuote = motivationalQuotesNorwegian[0];
+  let index = 0;
+
+  const updateQuote = () => {
+    currentQuote = motivationalQuotesNorwegian[index];
+    index = (index + 1) % motivationalQuotesNorwegian.length;
+  };
+
+  setInterval(updateQuote, 5000); // Update quote every 5 seconds
+</script>
+
+<div class="quote-container">
+  <div transition:fly={{ y: 0, duration: 1000 }}>
+    {currentQuote}
+  </div>
+</div>
+
+<style>
+  .quote-container {
+    position: relative;
+    overflow: hidden;
+  }
+</style>
